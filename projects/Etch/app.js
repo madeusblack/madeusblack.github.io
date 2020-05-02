03baa5f3e7e10b2a24cd9193121b64eb94a6a7a3
Yayson Olivares <44871312+madeusblack@users.noreply.github.com>
2020-05-01 23:16:20 -0400

    big rework
    -added projects folder with projects subfolders
    -reworked index to be more readable
    -added projects page
    -added contact page

---
+++ projects/Etch/app.js
@@ -0,0 +1,87 @@
+let totalgrid=0
+let test=document.documentElement.style.getPropertyValue("--trows");
+let test1=document.documentElement.style.getPropertyValue("--sizerows");
+let trows = document.querySelector('#rows').value;
+let contain = document.querySelector('.container')
+
+//-----------------------------------------------------
+function sizeOfRows(trows){
+    toret=440/trows
+    return toret
+}
+//-----------------------------------------------------
+function rowchange(){
+    console.log("test")
+    let size=sizeOfRows(trows)+"px"
+    document.documentElement.style.setProperty('--sizerows', size);
+    document.documentElement.style.setProperty('--trows', trows-1);
+
+}
+
+//----------------------------------------------------
+function colorset(){
+    let ascend = document.querySelector('#bwascendant').checked;
+    let rainbow = document.querySelector('#rainbow').checked;
+    let eraser = document.querySelector('#eraser').checked;
+
+    if(ascend==true){
+        console.log("grayscale")
+        let cellcolor= this.getAttribute("class")
+        let actualcolor=cellcolor.slice(9)
+        actualcolor=parseInt(actualcolor)
+        if(actualcolor==1){
+            actualcolor=11
+        setter="content c"+actualcolor
+        this.setAttribute('class', setter)
+        }
+        actualcolor=actualcolor-1
+        setter="content c"+actualcolor
+        this.setAttribute('class', setter)
+    }
+    if(rainbow==true){
+        let r=Math.floor((Math.random() * 255) + 1);
+        let g=Math.floor((Math.random() * 255) + 1);
+        let b=Math.floor((Math.random() * 255) + 1);
+        let rgbval="rgb("+r+" "+g+" "+b+")"
+        this.style.backgroundColor=rgbval
+    }
+    if(eraser==true){
+        this.style.backgroundColor=null
+        this.setAttribute('class', "content c11")
+    }
+}
+//-----------------------------------------------------
+function generate(){
+    contain = document.querySelector('.container')
+
+    if(document.querySelector(".content")!=null){
+        deleteChild()
+    }
+    trows = document.querySelector('#rows').value;
+    let tcells=trows*trows
+    rowchange()
+    test=document.documentElement.style.getPropertyValue("--trows");
+    test1=document.documentElement.style.getPropertyValue("--sizerows");
+    while (totalgrid<tcells){
+        const container = document.querySelector('.container');
+        let content = document.createElement('div');
+        content.classList.add('content');
+        content.classList.add('c11');
+        content.style.border= "1px solid black";
+        content.addEventListener("mouseover", colorset)
+        container.appendChild(content);
+        totalgrid++
+    }    tcells=0
+    totalgrid=0
+
+}
+//-----------------------------------------------------
+function deleteChild() { 
+    console.log("dchild")
+    contain = document.querySelector('.container')
+
+    while (contain.hasChildNodes()) {  
+        contain.removeChild(contain.firstChild);
+      }
+}
+window.onload = generate() ;
---
+++ projects/rockpaperscissors/app.js
@@ -0,0 +1,161 @@
+//--------------------------Global variables area----------------
+let games=0;
+let gwon=0;
+let glost=0;
+let gdraw=0;
+//--------------------------Functions area-----------------------
+function getIDsetHtml(id,inHtml)
+{
+    document.getElementById(id).innerHTML=inHtml
+}
+function getIDsetSrc(id,src)
+{
+    document.getElementById(id).src=src;
+}
+function setImages(playerSel,cpuSel)
+{
+    switch (true) {
+        case playerSel=="rock":
+            getIDsetSrc("playerGame","rock.png")
+            console.log("player"+playerSel)
+            break;
+        case playerSel=="paper":
+            getIDsetSrc("playerGame","paper.png")
+            console.log("player"+playerSel)
+            break; 
+        case playerSel=="scissors":
+            getIDsetSrc("playerGame","scissors.png")
+            console.log("player"+playerSel)
+            break;
+    }
+
+    switch (true) {
+        case cpuSel=="rock":
+            console.log("cpu")
+            getIDsetSrc("cpuGame","rock.png")
+            break;
+        case cpuSel=="paper":
+            console.log("cpu")
+            getIDsetSrc("cpuGame","paper.png")        
+            break; 
+        case cpuSel=="scissors":
+            console.log("cpu")
+            getIDsetSrc("cpuGame","scissors.png")            
+            break;
+        }
+}
+function resetGame()
+{
+    games=0;
+    gwon=0;
+    glost=0;
+    gdraw=0;
+    getIDsetHtml("gdraw",0);
+    getIDsetHtml("gtotal",0);
+    getIDsetHtml("glost",0);
+    getIDsetHtml("gwon",0);
+}
+
+function checkGameStatus(){
+if(games==5){
+    if(gwon==glost)
+        {
+            alert("It's a Drawn Game")
+            resetGame()
+        }
+        if(gwon>glost)
+        {
+            alert("Player Wins!")
+            resetGame()
+        }
+        if(gwon<glost)
+        {
+            alert("Cpu Wins!")
+            resetGame()
+        }
+    }
+}
+
+function computerPlay(){
+    let renum=Math.floor(Math.random() * 3);
+    let response="";
+    switch (renum) {
+        case 0:
+            response="rock"; 
+            break;
+        case 1:
+            response="paper";
+            break;
+        case 2:
+            response="scissors";
+            break;
+    }
+    return response
+}
+
+function updateBoard(result,tresults,tgames)
+{
+    switch (result) {
+        case "draw":
+            getIDsetHtml("gdraw",tresults)
+            getIDsetHtml("gtotal",tgames)
+            getIDsetHtml("resultGame","The Time (Draw Game)")
+            break;
+        case "cpuwon":
+            getIDsetHtml("glost",tresults)
+            getIDsetHtml("gtotal",tgames)
+            getIDsetHtml("resultGame","The Cpu")
+            break;
+        case "pwon":
+            getIDsetHtml("gwon",tresults)
+            getIDsetHtml("gtotal",tgames)
+            getIDsetHtml("resultGame","You , the Player")
+            break;
+    }
+
+}
+
+function game(computersel,PlayerSelection)
+{   
+    setImages(PlayerSelection,computersel)
+    switch(true) 
+    {
+        case PlayerSelection==computersel:
+            gdraw++;
+            games++;
+            console.log(gdraw)
+            updateBoard("draw",gdraw,games);
+            break;
+        case PlayerSelection=="rock"&&computersel=="paper":
+            glost++;
+            games++;
+            updateBoard("cpuwon",glost,games);
+            break;
+        case PlayerSelection=="scissors"&&computersel=="paper":
+            gwon++;
+            games++;
+            updateBoard("pwon",gwon,games);
+            break;
+        case PlayerSelection=="rock"&&computersel=="scissors":
+            glost++;
+            games++;
+            updateBoard("cpuwon",glost,games);
+            break;
+        case PlayerSelection=="paper"&&computersel=="scissors":
+            gwon++;
+            games++
+            updateBoard("pwon",gwon,games);
+            break;
+        case PlayerSelection=="scissors"&&computersel=="rock":
+            glost++;
+            games++
+            updateBoard("cpuwon",glost,games);
+            break;
+        case PlayerSelection=="paper"&&computersel=="rock":
+            gwon++;
+            games++;
+            updateBoard("pwon",gwon,games);
+            break;
+    }
+
+}
